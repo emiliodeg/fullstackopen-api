c5b34d4a2d3701fff08f163579b27b8233823e7d
Emilio Degiovanni <emilio_degiovanni@ryder.com>
2024-06-04 19:38:06 -0300

    3.9: cors added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 
 morgan.token("json-body", req => JSON.stringify(req.body));
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :json-body"));
 
@@ -82,5 +84,5 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
